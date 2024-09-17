8ffb5ad8c63d96d146772a175bf27d6440a4fe8e
Caden Cardell <cadencardell@gmail.com>
2024-09-17 14:42:27 -0400

    cleanup

--- DigitalSignal/__init__.py
+++ DigitalSignal/__init__.py
@@ -1,1 +1,8 @@
-from .digital_signal import DigitalSignal
+from .DigitalSignal import DigitalSignal
+
+# Making the module callable
+# __call__ = DigitalSignal
+
+
+def __call__():
+    print("hello world")
