7a8bec8745968a6782b8be2d1698cd2ff1f5dad5
Caden Cardell <cadencardell@gmail.com>
2024-10-04 06:44:08 -0400

    get item

--- tests/test_digital_signal.py
+++ tests/test_digital_signal.py
@@ -1,8 +1,26 @@
 # tests/test_digital_signal.py
 
 import pytest
-from DigitalSignal.digital_signal import DigitalSignal  # Assuming you have a class named DigitalSignal
+from DigitalSignal.digital_signal import DigitalSignal as DS # Assuming you have a class named DigitalSignal
+
+def test_signal_empty_initialization():
+    signal = DS()
+    assert signal[:] == [0]
 
 def test_signal_dirac():
-    signal = DigitalSignal([1])
+    signal = DS([1])
     assert signal[:] == [1]
+
+def test_signal_get_item():
+    signal = DS([1, 2, [3], 4, 5])
+
+    assert signal[-100] == 0
+    assert signal[-3] == 0
+    assert signal[-2] == 1
+    assert signal[-1] == 2
+    assert signal[0] == 3
+    assert signal[1] == 4
+    assert signal[2] == 5
+    assert signal[3] == 0
+    assert signal[100] == 0
+
