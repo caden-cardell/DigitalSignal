b4e3bcec291cc62199a74f573278a74bbcc2be2d
Caden Cardell <cadencardell@gmail.com>
2024-10-04 06:47:25 -0400

    set item test

--- tests/test_digital_signal.py
+++ tests/test_digital_signal.py
@@ -1,15 +1,16 @@
-# tests/test_digital_signal.py
-
 import pytest
 from DigitalSignal.digital_signal import DigitalSignal as DS # Assuming you have a class named DigitalSignal
+
 
 def test_signal_empty_initialization():
     signal = DS()
     assert signal[:] == [0]
 
+
 def test_signal_dirac():
     signal = DS([1])
     assert signal[:] == [1]
+
 
 def test_signal_get_item():
     signal = DS([1, 2, [3], 4, 5])
@@ -24,3 +25,12 @@ def test_signal_get_item():
     assert signal[3] == 0
     assert signal[100] == 0
 
+
+def test_signal_set_item():
+    signal = DS()
+
+    signal[-4] = 1
+    assert signal[:] == [1, 0, 0, 0, 0]
+
+    signal[2] = 3
+    assert signal[:] ==  [1, 0, 0, 0, 0, 0, 3]
